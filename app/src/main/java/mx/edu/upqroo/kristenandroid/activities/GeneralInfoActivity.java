7ff10479dc039b11cd0110daa5939ba214ecd5e1
Rodrigo Pech <rodrigoupech13@gmail.com>
2018-06-20 11:20:04 -0500

    Cambios en clase datos generales y layout

--- app/src/main/java/mx/edu/upqroo/kristenandroid/activities/GeneralInfoActivity.java
+++ app/src/main/java/mx/edu/upqroo/kristenandroid/activities/GeneralInfoActivity.java
@@ -14,6 +14,9 @@ public class GeneralInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_general_info);
-        mGeneralInfo = new GeneralInfo("Rodrigo", "Pech", "20", "Teibol dance", "201500100");
+        mGeneralInfo = new GeneralInfo("Rodrigo", "Pech", "Ing. Software", "201500100", "ISOF-2013 DE 375 CREDITOS", "( )",
+                                       "295.0", "VIGENTE", "(3153) SEP DIC 1", "00", "(09) (3182) MAY-AGO 18", "pepr970313hqrccd01",
+                                        "1997-03-13", "SANTA CECILIA", "CANCUN", "77500", "991564564", "9981158454", "rodrigoupech13@gmail.com",
+                                       "MANO AMIGA", "DIEGO LOVERA");
     }
 }
--- app/src/main/java/mx/edu/upqroo/kristenandroid/models/GeneralInfo.java
+++ app/src/main/java/mx/edu/upqroo/kristenandroid/models/GeneralInfo.java
@@ -3,20 +3,55 @@ package mx.edu.upqroo.kristenandroid.models;
 public class GeneralInfo {
     private String name;
     private String lastName;
-    private String age;
     private String career;
     private String enrollment;
+    private String planStudy;
+    private String modSpecial;
+    private String creditAcumm;
+    private String validity;
+    private String entryPeriiod;
+    private String validPeriods;
+    private String currentPeriod;
+    private String curp;
+    private String birthdate;
+    private String address;
+    private String city;
+    private String cp;
+    private String telDomicilio;
+    private String telCelular;
+    private String email;
+    private String originSchool;
+    private String tutor;
+
 
     public GeneralInfo() {
 
     }
 
-    public GeneralInfo(String name, String lastName, String age, String career, String enrollment) {
+    public GeneralInfo(String name, String lastName, String career, String enrollment, String planStudy, String modSpecial, String creditAcumm, String validity, String entryPeriiod,
+                       String validPeriods, String currentPeriod, String curp, String birthdate, String address, String city, String cp, String telDomicilio, String telCelular, String email,
+                       String originSchool, String tutor) {
         this.name = name;
         this.lastName = lastName;
-        this.age = age;
         this.career = career;
         this.enrollment = enrollment;
+        this.planStudy = planStudy;
+        this.modSpecial = modSpecial;
+        this.creditAcumm = creditAcumm;
+        this.validity = validity;
+        this.entryPeriiod = entryPeriiod;
+        this.validPeriods = validPeriods;
+        this.currentPeriod = currentPeriod;
+        this.curp = curp;
+        this.birthdate = birthdate;
+        this.address = address;
+        this.city = city;
+        this.cp = cp;
+        this.telDomicilio = telDomicilio;
+        this.telCelular = telCelular;
+        this.email = email;
+        this.originSchool = originSchool;
+        this.tutor = tutor;
     }
 
     public String getName() {
@@ -35,14 +70,6 @@ public class GeneralInfo {
         this.lastName = lastName;
     }
 
-    public String getAge() {
-        return age;
-    }
-
-    public void setAge(String age) {
-        this.age = age;
-    }
-
     public String getCareer() {
         return career;
     }
@@ -57,5 +84,141 @@ public class GeneralInfo {
 
     public void setEnrollment(String enrollment) {
         this.enrollment = enrollment;
+    }
+
+    public String getPlanStudy() {
+        return planStudy;
+    }
+
+    public void setPlanStudy(String planStudy) {
+        this.planStudy = planStudy;
+    }
+
+    public String getModSpecial() {
+        return modSpecial;
+    }
+
+    public void setModSpecial(String modSpecial) {
+        this.modSpecial = modSpecial;
+    }
+
+    public String getCreditAcumm() {
+        return creditAcumm;
+    }
+
+    public void setCreditAcumm(String creditAcumm) {
+        this.creditAcumm = creditAcumm;
+    }
+
+    public String getValidity() {
+        return validity;
+    }
+
+    public void setValidity(String validity) {
+        this.validity = validity;
+    }
+
+    public String getEntryPeriiod() {
+        return entryPeriiod;
+    }
+
+    public void setEntryPeriiod(String entryPeriiod) {
+        this.entryPeriiod = entryPeriiod;
+    }
+
+    public String getValidPeriods() {
+        return validPeriods;
+    }
+
+    public void setValidPeriods(String validPeriods) {
+        this.validPeriods = validPeriods;
+    }
+
+    public String getCurrentPeriod() {
+        return currentPeriod;
+    }
+
+    public void setCurrentPeriod(String currentPeriod) {
+        this.currentPeriod = currentPeriod;
+    }
+
+    public String getCurp() {
+        return curp;
+    }
+
+    public void setCurp(String curp) {
+        this.curp = curp;
+    }
+
+    public String getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(String birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCp() {
+        return cp;
+    }
+
+    public void setCp(String cp) {
+        this.cp = cp;
+    }
+
+    public String getTelDomicilio() {
+        return telDomicilio;
+    }
+
+    public void setTelDomicilio(String telDomicilio) {
+        this.telDomicilio = telDomicilio;
+    }
+
+    public String getTelCelular() {
+        return telCelular;
+    }
+
+    public void setTelCelular(String telCelular) {
+        this.telCelular = telCelular;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getOriginSchool() {
+        return originSchool;
+    }
+
+    public void setOriginSchool(String originSchool) {
+        this.originSchool = originSchool;
+    }
+
+    public String getTutor() {
+        return tutor;
+    }
+
+    public void setTutor(String tutor) {
+        this.tutor = tutor;
     }
 }
