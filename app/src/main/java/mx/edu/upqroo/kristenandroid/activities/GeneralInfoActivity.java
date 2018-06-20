b632e56687935129142846240cc8c109d2b17b18
Rodrigo Pech <rodrigoupech13@gmail.com>
2018-06-20 12:14:06 -0500

    Se vincularon los textview con las variables

--- app/src/main/java/mx/edu/upqroo/kristenandroid/activities/GeneralInfoActivity.java
+++ app/src/main/java/mx/edu/upqroo/kristenandroid/activities/GeneralInfoActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 import mx.edu.upqroo.kristenandroid.R;
 import mx.edu.upqroo.kristenandroid.models.GeneralInfo;
 
@@ -21,5 +23,62 @@ public class GeneralInfoActivity extends AppCompatActivity {
 
         TextView alumnName = findViewById(R.id.text_alumn_name);
         alumnName.setText(mGeneralInfo.getName());
+
+        TextView alumnLastName = findViewById(R.id.text_alumn_lastname);
+        alumnLastName.setText(mGeneralInfo.getLastName());
+
+        TextView alumnCareer = findViewById(R.id.text_alumn_career);
+        alumnCareer.setText(mGeneralInfo.getCareer());
+
+        TextView alumnPlanstudy = findViewById(R.id.text_alumn_studieplan);
+        alumnPlanstudy.setText(mGeneralInfo.getPlanStudy());
+
+        TextView alumnModspecial = findViewById(R.id.text_alumn_speciality);
+        alumnModspecial.setText(mGeneralInfo.getModSpecial());
+
+        TextView alumnCredits = findViewById(R.id.text_alumn_credits);
+        alumnCredits.setText(mGeneralInfo.getCreditAcumm());
+
+        TextView alumnValidity = findViewById(R.id.text_alumn_status);
+        alumnValidity.setText(mGeneralInfo.getValidity());
+
+        TextView alumnEntryP = findViewById(R.id.text_alumn_starting_period);
+        alumnEntryP.setText(mGeneralInfo.getEntryPeriiod());
+
+        TextView alumnValidp = findViewById(R.id.text_alumn_valid_periods);
+        alumnValidp.setText(mGeneralInfo.getValidPeriods());
+
+        TextView alumnCurrentp = findViewById(R.id.text_alumn_actual_period);
+        alumnCurrentp.setText(mGeneralInfo.getCreditAcumm());
+
+        TextView alumnCurp = findViewById(R.id.text_alumn_curp);
+        alumnCurp.setText(mGeneralInfo.getCurp());
+
+        TextView alumnBirth = findViewById(R.id.text_alumn_birth);
+        alumnBirth.setText(mGeneralInfo.getBirthdate());
+
+        TextView alumnAddress = findViewById(R.id.text_alumn_address);
+        alumnAddress.setText(mGeneralInfo.getAddress());
+
+        TextView alumnCity = findViewById(R.id.text_alumn_city);
+        alumnCity.setText(mGeneralInfo.getCity());
+
+        TextView alumnCP = findViewById(R.id.text_alumn_postal_code);
+        alumnCP.setText(mGeneralInfo.getCp());
+
+        TextView alumnTelDom = findViewById(R.id.text_alumn_local_phone);
+        alumnTelDom.setText(mGeneralInfo.getTelDomicilio());
+
+        TextView alumnTelCel = findViewById(R.id.text_alumn_mobile_phone);
+        alumnTelCel.setText(mGeneralInfo.getTelCelular());
+
+        TextView alumnEmail = findViewById(R.id.text_alumn_email);
+        alumnEmail.setText(mGeneralInfo.getEmail());
+
+        TextView alumnSchool = findViewById(R.id.text_alumn_origin_school);
+        alumnSchool.setText(mGeneralInfo.getOriginSchool());
+
+        TextView alumnTutor = findViewById(R.id.text_alumn_tutor);
+        alumnTutor.setText(mGeneralInfo.getTutor());
     }
 }
