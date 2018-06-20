98843121aab9dc5f2df4003b2e40c84e0a6134a4
DiegoLovera <36526771+DiegoLovera@users.noreply.github.com>
2018-06-20 12:15:32 -0500

    Ajuste visual en el generalInfoActivity

--- app/src/main/java/mx/edu/upqroo/kristenandroid/activities/NewsActivity.java
+++ app/src/main/java/mx/edu/upqroo/kristenandroid/activities/NewsActivity.java
@@ -114,6 +114,7 @@ public class NewsActivity extends AppCompatActivity
 
         if (id == R.id.nav_camera) {
             // Handle the camera action
+            startActivity(new Intent(this, GeneralInfoActivity.class));
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
