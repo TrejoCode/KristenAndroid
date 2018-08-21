c35b578761ac34332f439a679381945474b0b6b1
DiegoLovera <36526771+DiegoLovera@users.noreply.github.com>
2018-08-20 21:15:42 -0500

    Version changed and orientation loked

--- app/src/main/java/mx/edu/upqroo/kristenandroid/activities/LoginActivity.java
+++ app/src/main/java/mx/edu/upqroo/kristenandroid/activities/LoginActivity.java
@@ -36,6 +36,7 @@ public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme_NoActionBar);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
