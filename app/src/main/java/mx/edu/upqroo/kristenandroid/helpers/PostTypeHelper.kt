579b28528d90bc975a3141772ede2ce4a92c5e1d
Diego Lovera <diegolovera010@gmail.com>
2019-06-19 09:34:21 -0500

    PostTypeHelper -> ktx

---
+++ app/src/main/java/mx/edu/upqroo/kristenandroid/helpers/PostTypeHelper.kt
@@ -0,0 +1,12 @@
+package mx.edu.upqroo.kristenandroid.helpers
+
+/**
+ * <h1>PostTypeHelper</h1>
+ * This is used to separate the different kinds of post that can be done.
+ */
+enum class PostTypeHelper(val id: Int) {
+    EVENT(1),
+    NEWS(2),
+    WORK(3),
+    PLANT(4)
+}
